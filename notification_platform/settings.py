b1d3b463b027ee4d205cc10be63932bdc6abc7de
Aditi <aditi22mehta22@gmail.com>
2025-09-27 23:46:13 +0530

    update

--- notification_platform/settings.py
+++ notification_platform/settings.py
@@ -92,7 +92,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'https://*.onrender.com',  
-
+]
 ROOT_URLCONF = 'notification_platform.urls'
 
 TEMPLATES = [
